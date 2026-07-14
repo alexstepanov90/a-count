642bcc2ba4d2f0e962517ebe73229de65b637c11
alexstepanov90 <a.a.stepanov90@gmail.com>
2026-07-14 22:38:49 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// Сборка в ОДИН html-файл: работает двойным кликом, без установки и без сервера
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   base: '/a-count/',
 })
